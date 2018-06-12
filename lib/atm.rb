cb0ea585128bdf5a867988d424e4204184393221
Erik <erik.steinman@hotmail.com>
2018-06-12 09:18:56 +0200

    WIP failed ID-code on Datetime

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class ATM
     attr_accessor :funds
     
@@ -5,7 +7,14 @@ class ATM
         @funds = 1000
     end
 
-    def withdraw(amount)
+    def withdraw(amount, account)
+        case
+        when amount > account.balance
+            return
+        else
         @funds -= amount
+        account.balance = account.balance - amount
+        { status: true, message: 'success', date: DateTime.now, amount: amount }
+        end 
     end
 end
