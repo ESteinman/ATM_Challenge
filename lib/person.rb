153093b38803483d2c1a88722edcf506e492bc32
Erik <erik.steinman@hotmail.com>
2018-06-13 11:34:39 +0200

    Modify person.rb and person_spec.rb

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,21 @@
-
-
 class Person
-    attr_accessor :name, :cash, :account
 
-    def initialize
-        @name = 'Thomas'
+    attr_accessor :name, :cash
+
+    def initialize(attrs = {})
+        set_name(attrs[:name])
         @cash = 0
-        @account = nil
+    end
+    
+    private
+    def set_name(name)
+        name == nil ?  no_name_is_set : @name = name
+    end
 
-def 
+    def no_name_is_set
+        raise "A name is required"
+    end
+
+
+
+end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,40 +1,21 @@
 require './lib/person'
 require './lib/atm'
 
-describe Person do
+    describe Person do
 
-    subject { described_class.new(name: 'Thomas') }
+        subject { described_class.new(name: 'Thomas') }
 
-    it 'is expected to have a :name on initialize' do
-        expect(subject.name).not_to be nil
-    end
-
-    it 'is expected to raise error if no name is set' do
-        expect { described_class.new}.to raise_error 'A name is required'
-    end
-
-    it 'is expected to have a :cash attribute with value of 0 on initialize' do
-        expect(subject.cash).to eq 0
-    end
-
-    it 'is expected to have a :account attribute' do
-        expect(subject.account).to be nil
-    end
-
-    describe 'can create and Account' do
-        before { subject.create_account }
-        it 'of Account class' do
-            expect(subject.account).to be_an_instance_of Account
+        it 'is expected to have a :name on initialize' do
+            expect(subject.name).not_to be nil
         end
-    
-    it 'with himself as an owner' do
-         expect(subject.owner).to be subject
-    end
 
-    describe 'can not manage funds if account been created' do
-        it 'can\'t deposit funds' do
-            expect { subject.deposit(100)}.to raise_error(RuntimeError, 'No account present')
+        it 'is expected to raise error if no name is set' do
+            expect { described_class.new }.to raise_error 'A name is required'
         end
+
+        it 'is expected to have a :cash attribute with value of 0 on initialize' do
+            expect(subject.cash).to eq 0
+        end
+
+        
     end
-end
-end
